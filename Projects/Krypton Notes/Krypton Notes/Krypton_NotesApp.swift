5d45ac52a0fc786687a1975279777d078794d6eb
dunamismax <dunamismax@tutamail.com>
2025-06-28 12:17:28 -0400

    Krypton Notes for MacOS

---
+++ Projects/Krypton Notes/Krypton Notes/Krypton_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Krypton_NotesApp.swift
+//  Krypton Notes
+//
+//  Created by Stephen Sawyer on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Krypton_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
